a235207e2e6a4b6bbf543079d44dede30dc4104a
truonggiang2511a-droid <truonggiang2511a@gmail.com>
2026-08-25 04:53:13 -0700

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const url = import.meta.env.VITE_SUPABASE_URL;
-const key = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
-export const supabase =
-  url && key ? createClient(url, key) : null;
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+);
